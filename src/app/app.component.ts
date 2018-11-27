08f92c410adab05547c89db6b609015bda9a8479
Aissa BOUGUERN <aissa.bouguern@gmail.com>
2018-11-27 17:08:13 +0000

    Angular Services Tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from './services/post.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers : [PostService]
 })
-export class AppComponent {
-  title = 'angular-routing';
+export class AppComponent implements OnInit {
+  	public posts: any;
+
+  	constructor (private postService: PostService) {
+
+  	}
+
+ 	ngOnInit() {
+ 		this.getPosts();
+ 	}
+
+  	getPosts(){
+  		this.postService.fetchPosts().subscribe(data => {
+  			
+  			this.posts = data;
+  			
+  		});
+  	}
 }
+4
