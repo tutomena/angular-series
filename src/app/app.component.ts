6a25ad6d5525cd7bca15e3bb53f523cffd19d6fe
Aissa BOUGUERN <aissa.bouguern@gmail.com>
2018-11-27 17:50:24 +0000

    Angular Services Tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,7 @@ import { PostService } from './services/post.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers : [PostService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   	public posts: any;
